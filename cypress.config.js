87b7b23b8ed3f0d84b9442530e84c3342a3cd82e
Yeferson castiblanco <yefersoncasti@gmail.com>
2024-04-03 21:22:13 -0500

    last commit

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
 
   e2e: {
     // TODO: No hay que dejar espacios en la url
-    baseUrl: 'https://pideenlinea.lenosycarbon.com.co/',
+    baseUrl: 'https://www.diluccatogo.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {
